93517c291921cad79067ec695404a7084fc0aa1d
Sam Wronski <runewake2@outlook.com>
2016-11-07 21:38:25 -0600

    Fix broken test.

--- tests/WorldOfZero.GameJolt.Tests/Class1Tests.cs
+++ tests/WorldOfZero.GameJolt.Tests/Class1Tests.cs
@@ -15,12 +15,6 @@ namespace WorldOfZero.GameJolt.Tests
             Assert.Equal(4, Add(2, 2));
         }
 
-        [Fact]
-        public void FailingTest()
-        {
-            Assert.Equal(5, Add(2, 2));
-        }
-
         int Add(int x, int y)
         {
             return x + y;
--- tests/WorldOfZero.GameJolt.Tests/Properties/AssemblyInfo.cs
+++ tests/WorldOfZero.GameJolt.Tests/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("24f7bb29-ba01-43a5-9cf2-b91767ee9ba1")]
+[assembly: Guid("42433885-f3a6-4e63-8169-34c00ba740b1")]
 
 // Version information for an assembly consists of the following four values:
 //
