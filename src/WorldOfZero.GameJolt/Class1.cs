3b0468f0656ffba652361c94eb680145d48676da
Sam Wronski <runewake2@outlook.com>
2016-11-07 20:58:16 -0600

    Changed from .NET Core to .NET for real this time. :D

--- src/WorldOfZero.GameJolt/Class1.cs
+++ src/WorldOfZero.GameJolt/Class1.cs
@@ -1,14 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
+using System.Text;
 
 namespace WorldOfZero.GameJolt
 {
     public class Class1
     {
-        public Class1()
-        {
-        }
     }
 }
